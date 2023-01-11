7ce6e602c91b69f0bd7f5d1624287d8374e5dbe5
unknown <easystudybd20@gmail.com>
2023-01-11 20:41:11 +0600

    a

--- src/Home/Projects/Projects.js
+++ src/Home/Projects/Projects.js
@@ -5,7 +5,58 @@ const Projects = () => {
         <div>
             <h2 class="text-center text-2xl font-bold">Projects</h2>
             <br></br>
+
+            {/* ..............................................Tutora Projects............................................ */}
+
+            
             <h2 class ="text-center text-xl">1.Tutora </h2><br></br>
+           <div class="grid lg:grid-cols-3 ml-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
+           <div class="card w-96" >
+  <img class="card-img-top" src="/images/qualityeducation.gif" alt="Faileed to load"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-primary" type="submit"><a href="https://play.google.com/store/apps/details?id=com.Tutora">DOWNLOAD TUTORA</a></button>
+   
+  </div>
+</div>
+<div class="card w-96" >
+  <img class="card-img-top" src="/images/teacher.gif" alt="Faileed to load" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-primary" type="submit"><a href="https://play.google.com/store/apps/details?id=com.Tutora">DOWNLOAD TUTORA</a></button>
+  </div>
+</div>
+<div class="card w-96" >
+  <img class="card-img-top" src="images/mike.gif" alt="Faileed to load"  />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-primary" type="submit"><a href="https://play.google.com/store/apps/details?id=com.Tutora">DOWNLOAD TUTORA</a></button>
+  </div>
+</div>
+<div class="card w-96" >
+  <img class="card-img-top" src="images/lader.gif" alt="Faileed to load" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-primary" type="submit"><a href="https://play.google.com/store/apps/details?id=com.Tutora">DOWNLOAD TUTORA</a></button>
+  </div>
+</div>
+<div class="card w-96" >
+  <img class="card-img-top" src="images/handshake.gif"alt="Faileed to load"  />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-primary" type="submit"><a href="https://play.google.com/store/apps/details?id=com.Tutora">DOWNLOAD TUTORA</a></button>
+  </div>
+</div>
+
+
+           </div>
+           <br></br><br></br>
+            <h2 class ="text-center text-xl">2.E-Commerce </h2><br></br>
            <div class="grid lg:grid-cols-3 ml-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
            <div class="card w-96" >
   <img class="card-img-top" src="/images/qualityeducation.gif" alt="Faileed to load"/>
